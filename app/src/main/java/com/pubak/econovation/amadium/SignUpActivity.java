69b86c96c343f9071ec1727a3686b03cd53ca2eb
Bellroute <kjkun7631@naver.com>
2018-11-27 21:10:48 +0900

    refactor: refactoring regex about email&password

--- app/src/main/java/com/pubak/econovation/amadium/SignUpActivity.java
+++ app/src/main/java/com/pubak/econovation/amadium/SignUpActivity.java
@@ -3,17 +3,19 @@ package com.pubak.econovation.amadium;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$");     // 비밀번호 정규식(최소 8자리에 숫자, 문자, 특수문자가 1개 이상 포함)
     private FirebaseAuth firebaseAuth;
     private EditText editTextEmail;
     private EditText editTextPassword;
@@ -35,13 +37,39 @@ public class SignUpActivity extends AppCompatActivity {
         buttonJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!editTextEmail.getText().toString().equals("") && !editTextPassword.getText().toString().equals("")) {
+                if (!isValidEmail() && !isValidPassword()) {
+                    // 이메일과 비밀번호를 잘 입력한 경우
                     createUser(editTextEmail.getText().toString(), editTextPassword.getText().toString(), editTextName.getText().toString());
                 } else {
+                    // 이메일과 비밀번호가 공백인 경우
                     Toast.makeText(SignUpActivity.this, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+
+    // 이메일 유효성검사 메소드
+    private boolean isValidEmail() {
+        if (editTextEmail.getText().toString().isEmpty()) {
+            // 이메일이 공백인 경우
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(editTextEmail.getText().toString()).matches()) {
+            // 잘못된 메일 형식인 경우
+            return false;
+        } else
+            return true;
+    }
+
+    // 비밀번호 유효성검사 메소드
+    private boolean isValidPassword() {
+        if (editTextPassword.getText().toString().isEmpty()) {
+            // 비밀번호가 공백인 경우
+            return false;
+        } else if (!PASSWORD_PATTERN.matcher(editTextPassword.getText().toString()).matches()) {
+            // 잘못된 비밀번호 형식인 경우
+            return false;
+        } else
+            return true;
     }
 
     private void createUser(String email, String password, String name) {
@@ -50,6 +78,7 @@ public class SignUpActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            // 회원가입 성공시
                             Toast.makeText(SignUpActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
                             finish();
                         } else {
