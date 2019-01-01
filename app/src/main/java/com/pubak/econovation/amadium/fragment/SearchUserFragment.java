0ee0c0515062276c58ff75e16d7ad8f93df073c4
Bellroute <kjkun7631@gmail.com>
2019-01-01 16:54:27 +0900

    refactor: bottomNavigationBar -> tabLayOut

--- app/src/main/java/com/pubak/econovation/amadium/fragment/SearchUserFragment.java
+++ app/src/main/java/com/pubak/econovation/amadium/fragment/SearchUserFragment.java
@@ -1,15 +1,21 @@
 package com.pubak.econovation.amadium.fragment;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.pubak.econovation.amadium.R;
 
-public class SearchUserFragment extends AppCompatActivity {
+public class SearchUserFragment extends Fragment {
 
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_search_user);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search_user, container, false);
     }
 }
