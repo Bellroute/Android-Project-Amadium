90ced36fc8d9d11a1b97630ae9af78d91f9e4d27
Bellroute <kjkun7631@gmail.com>
2019-01-01 16:57:02 +0900

    feat: TabPagerAdapter.class

---
+++ app/src/main/java/com/pubak/econovation/amadium/adapter/TabPagerAdapter.java
@@ -0,0 +1,21 @@
+package com.pubak.econovation.amadium.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TabPagerAdapter extends FragmentStatePagerAdapter {
+    public TabPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
