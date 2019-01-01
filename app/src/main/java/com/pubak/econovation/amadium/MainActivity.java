3f90aa4e4d941ef8dce733b0f41d47e445bc334a
Bellroute <kjkun7631@gmail.com>
2019-01-01 16:34:03 +0900

    refactor: ListFragment -> Fragment

--- app/src/main/java/com/pubak/econovation/amadium/MainActivity.java
+++ app/src/main/java/com/pubak/econovation/amadium/MainActivity.java
@@ -3,11 +3,9 @@ package com.pubak.econovation.amadium;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
@@ -23,13 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SearchUserFragment searchUserFragment = (SearchUserFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MatchListFragment()).commit();
-
-
         userName = findViewById(R.id.user_view);
-        userName.setText(userEmail.substring(0,userEmail.lastIndexOf("@")));
+        userName.setText(userEmail.substring(0, userEmail.lastIndexOf("@")));
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationListener);
--- app/src/main/java/com/pubak/econovation/amadium/ResultListFragment.java
+++ app/src/main/java/com/pubak/econovation/amadium/ResultListFragment.java
@@ -3,12 +3,13 @@ package com.pubak.econovation.amadium;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ResultListFragment extends ListFragment {
+public class ResultListFragment extends Fragment {
 
     @Nullable
     @Override
