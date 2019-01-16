747bb1f391c72e6e086db0bd637d8bf5985661e8
Bellroute <kjkun7631@gmail.com>
2019-01-16 20:09:05 +0900

    feat: google map user marker

--- app/src/main/java/com/pubak/econovation/amadium/fragment/SearchUserFragment.java
+++ app/src/main/java/com/pubak/econovation/amadium/fragment/SearchUserFragment.java
@@ -1,23 +1,27 @@
 package com.pubak.econovation.amadium.fragment;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -25,7 +29,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.pubak.econovation.amadium.R;
 import com.pubak.econovation.amadium.activity.MainActivity;
-import com.pubak.econovation.amadium.adapter.RecyclerViewAdapter;
 import com.pubak.econovation.amadium.dto.UserDTO;
 
 import java.util.ArrayList;
@@ -35,11 +38,17 @@ public class SearchUserFragment extends Fragment implements OnMapReadyCallback {
     private static final String TAG = "SearchUserFragment";
     private GoogleMap map;
     private MapView mapView;
+    private List<UserDTO> dataSet;
+    private UserDTO user;
+    private FirebaseDatabase firebaseDatabase;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search_user, container, false);
+        dataSet = new ArrayList<>();
+        firebaseDatabase = firebaseDatabase.getInstance();
+
         return view;
     }
 
@@ -49,28 +58,100 @@ public class SearchUserFragment extends Fragment implements OnMapReadyCallback {
         map = googleMap;
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
+        initDatabase();
+    }
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
-
+    private void getMarkers(UserDTO userDTO) {
+        Log.d(TAG, "getMarkers: userDTO" + userDTO);
+        LatLng markValue = new LatLng(userDTO.getLatitude(), userDTO.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        map.addMarker(markerOptions);
 
-        map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        map.animateCamera(CameraUpdateFactory.zoomTo(11));
+        markerOptions.position(markValue)
+                .title(userDTO.getUsername())
+                .snippet("스포츠: " + userDTO.getSport() + "\n티어: " + userDTO.getTier());
+
+        if (userDTO.getEmail().equals(MainActivity.getCurrentUser().getEmail())) {
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        }
+        map.addMarker(markerOptions).showInfoWindow();
+
+        map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                LinearLayout info = new LinearLayout(getContext());
+                info.setOrientation(LinearLayout.VERTICAL);
+
+                TextView title = new TextView(getContext());
+                title.setTextColor(Color.BLACK);
+                title.setGravity(Gravity.CENTER);
+                title.setTypeface(null, Typeface.BOLD);
+                title.setText(marker.getTitle());
+
+                TextView snippet = new TextView(getContext());
+                snippet.setTextColor(Color.GRAY);
+                snippet.setText(marker.getSnippet());
+
+                info.addView(title);
+                info.addView(snippet);
+
+                return info;
+            }
+        });
+
+        if (userDTO.getEmail().equals(MainActivity.getCurrentUser().getEmail())) {
+            user = userDTO;
+
+            map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(user.getLatitude(), user.getLongitude())));
+            map.animateCamera(CameraUpdateFactory.zoomTo(14));
+        }
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         mapView = (MapView) view.findViewById(R.id.map);
         if (mapView != null) {
             mapView.onCreate(null);
             mapView.onResume();
             mapView.getMapAsync(this);
         }
+    }
+
+    private void initDatabase() {
+        firebaseDatabase.getReference().child("users").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                UserDTO userDTO = dataSnapshot.getValue(UserDTO.class);
+                dataSet.add(userDTO);
+                Log.d(TAG, "onChildAdded: userDTO.getEmail(): " + userDTO.getEmail());
+                getMarkers(userDTO);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
